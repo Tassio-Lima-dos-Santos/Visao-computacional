02aa09c844f13653fc764072f28e7baf28dfb09d
Tassio-Lima-dos-Santos <tassiolimasf@gmail.com>
2025-10-09 15:51:26 -0300

    Aula 13 - Parte 1

---
+++ Aula13_Operacoes_Morfologicas-remocao-elementos-borda-2.py
@@ -0,0 +1,15 @@
+import cv2
+from matplotlib import pyplot as plt
+import numpy as np
+import visaoComputacional as visco
+
+I = cv2.imread('./Imagens/binary_dots.jpg', cv2.IMREAD_GRAYSCALE)
+ret, I = cv2.threshold(I, 0, 255, cv2.THRESH_BINARY+cv2.THRESH_OTSU)
+plt.figure()
+plt.imshow(I, cmap='gray')
+
+I_final = visco.imclearboard(I)
+plt.figure()
+plt.imshow(I_final, cmap='gray')
+
+plt.show()
---
+++ Aula13_Operacoes_Morfologicas-remocao-elementos-borda.py
@@ -0,0 +1,28 @@
+import cv2
+from matplotlib import pyplot as plt
+import numpy as np
+import visaoComputacional as visco
+
+I = cv2.imread('./Imagens/binary_dots.jpg', cv2.IMREAD_GRAYSCALE)
+ret, I = cv2.threshold(I, 0, 255, cv2.THRESH_BINARY+cv2.THRESH_OTSU)
+plt.figure()
+plt.imshow(I, cmap='gray')
+
+I_mascara = I.copy()
+# plt.figure()
+# plt.imshow(I_mascara, cmap='gray')
+
+I_marcador = I_mascara.copy()
+I_marcador[2:-1,2:-1] = 0
+# plt.figure()
+# plt.imshow(I_marcador, cmap='gray')
+
+I_reconstruida = visco.imreconstruction(I_mascara, I_marcador)
+# plt.figure()
+# plt.imshow(I_reconstruida, cmap='gray')
+
+I_final = I - I_reconstruida
+plt.figure()
+plt.imshow(I_final, cmap='gray')
+
+plt.show()
