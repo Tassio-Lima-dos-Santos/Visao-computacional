9cb642c93a5e4fafcb79ee61989ada15c01f8fe4
Tassio-Lima-dos-Santos <tassiolimasf@gmail.com>
2025-10-09 15:54:45 -0300

    Aula 13 - Parte 2

--- Aula12_Operacoes_Morfologicas-preenchimento-2.py
+++ Aula12_Operacoes_Morfologicas-preenchimento-2.py
@@ -5,24 +5,10 @@ import visaoComputacional as visco
 
 I = cv2.imread('./Imagens/binary_dots.jpg', cv2.IMREAD_GRAYSCALE)
 ret, I = cv2.threshold(I, 0, 255, cv2.THRESH_BINARY+cv2.THRESH_OTSU)
-# plt.figure()
-# plt.imshow(I, cmap='gray')
-
-# Inversão da imagem binária
-I_mascara = 255 - I
 plt.figure()
-plt.imshow(I_mascara, cmap='gray')
+plt.imshow(I, cmap='gray')
 
-I_marcador = I_mascara.copy()
-I_marcador[2:-1,2:-1] = 0
-plt.figure()
-plt.imshow(I_marcador, cmap='gray')
-
-I_reconstruida = visco.imreconstruction(I_mascara, I_marcador)
-plt.figure()
-plt.imshow(I_reconstruida, cmap='gray')
-
-I_final = 255 - I_reconstruida
+I_final = visco.imfill(I)
 plt.figure()
 plt.imshow(I_final, cmap='gray')
 
