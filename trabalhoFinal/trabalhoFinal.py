10bfe5215ae3ed71999fab5417e7cf8c223b47fe
Tassio-Lima-dos-Santos <tassiolimasf@gmail.com>
2025-12-03 01:27:09 -0300

    Trabalho Final parcialmente funcional - Uso de característica de contornos

--- trabalhoFinal/trabalhoFinal.py
+++ trabalhoFinal/trabalhoFinal.py
@@ -3,45 +3,134 @@ import numpy as np
 from matplotlib import pyplot as plt
 
 # Parâmetros do algoritmo
-imagem_de_entrada = './trabalhoFinal/banco_de_imagens/nível 1/placa2.jpg'
+imagem_de_entrada = './trabalhoFinal/banco_de_imagens/nível 1/placa1.jpg'
+
+# Canny
+t_upper = 600
+t_lower = 100
+
+# Transformação morfológica
+structElem = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (5,5))
+
+# Filtragem de contornos
+minArea = 1000
 
 # Inicialização das imagens
 I_input = cv2.imread(imagem_de_entrada)
+I_output = I_input.copy()
 I_reference = cv2.imread('./trabalhoFinal/banco_de_imagens/nível 1/placa1.jpg')
 I_template = cv2.imread('./trabalhoFinal/banco_de_imagens/fonte_mercosul.png')
 
-# Ajuste de perspectiva usando o detector e descritor SIFT
-# Instancia o detector e descritor SIFT
-sift = cv2.SIFT_create()
+# Tamanho da imagem da placa tratada
+# W, H = (400,200)
 
-# Acha os pontos de interesses e seus descritores das imagens de referência e entrada
-kp_reference, desc_reference = sift.detectAndCompute(I_reference, None)
-kp_input, desc_input = sift.detectAndCompute(I_input, None)
+def sort_points(pts):
+    # pts: array (4,1,2)
+    pts = pts.reshape(4, 2)
 
-# Cria objeto BFMatcher
-bf = cv2.BFMatcher(cv2.NORM_L2, crossCheck=False)
+    # ordena por soma (x+y) e diferença (x-y)
+    soma = pts.sum(axis=1)
+    diff = np.diff(pts, axis=1).reshape(4)
 
-# Executa o match entre os descritores da imagem de referência e de entrada
-matches = bf.match(desc_input, desc_reference)
+    p0 = pts[np.argmin(soma)]   # top-left
+    p2 = pts[np.argmax(soma)]   # bottom-right
+    p1 = pts[np.argmin(diff)]   # top-right
+    p3 = pts[np.argmax(diff)]   # bottom-left
 
-# Ordena os matches pela menor distância
-matches = sorted(matches, key = lambda x:x.distance)
-N = len(matches)
+    return np.array([p0, p1, p2, p3], dtype="float32")
 
-# Obter matriz de homografia que mapeia a imagem de entrada na imagem de referência
-src_pts = np.zeros((N, 1, 2), np.float32)
-dst_pts = np.zeros((N, 1, 2), np.float32)
+def tamanho_da_placa(pts):
+    (p0, p1, p2, p3) = pts
 
-for i, match in enumerate(matches):
-    src_pts[i] = np.float32(kp_input[match.queryIdx].pt).reshape(-1, 1, 2)
-    dst_pts[i] = np.float32(kp_reference[match.trainIdx].pt).reshape(-1, 1, 2)
+    largura1 = np.linalg.norm(p1 - p0)
+    largura2 = np.linalg.norm(p2 - p3)
+    largura = max(int(largura1), int(largura2))
 
-# Matriz de homografia
-M, mask = cv2.findHomography(src_pts, dst_pts, method=cv2.RANSAC)
+    altura1 = np.linalg.norm(p3 - p0)
+    altura2 = np.linalg.norm(p2 - p1)
+    altura = max(int(altura1), int(altura2))
 
-height, width, _ = I_reference.shape
+    return largura, altura
 
-I_ajusted = cv2.warpPerspective(I_input, M, (width, height))
+# Ajuste de perspectiva usando detecção de bordas e características de contorno
+
+# Aumento de nitidez
+
+# # Filtragem 2D pura
+# k_sharp = np.array([
+#     [ 0, -1,  0],
+#     [-1,  5, -1],
+#     [ 0, -1,  0]
+# ])
+# I_sharp = cv2.filter2D(I_input, -1, k_sharp)
+
+# Unsharp mask
+I_blur = cv2.GaussianBlur(I_input, (0,0), sigmaX=5)
+I_sharp = cv2.addWeighted(I_input, 1.8, I_blur, -0.8, 0)
+
+I_output = I_sharp
+
+# Detecção de bordas com Canny
+I_border = cv2.Canny(I_sharp, t_upper, t_lower)
+
+# Fechamento morfológico para melhorar a qualidade dos contornos
+I_border = cv2.morphologyEx(I_border, cv2.MORPH_CLOSE, structElem)
+
+# Aquisição dos contornos
+contours, _ = cv2.findContours(I_border, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+
+filtered_contours = []
+
+for cnt in contours:
+    # Filtro de área mínima
+    area = cv2.contourArea(cnt)
+    if area < minArea:
+        continue
+
+    # Aproximação poligonal (para achar quadrilátero)
+    peri = cv2.arcLength(cnt, True)
+    approx = cv2.approxPolyDP(cnt, 0.03 * peri, True)
+
+    # Verifica se é quadrilátero
+    if len(approx) != 4:
+        continue    
+
+    # Retangularidade comparando a área à área do menor retângulo possível
+    rect = cv2.minAreaRect(cnt)
+    box = cv2.boxPoints(rect)
+    box = np.intp(box)
+    retangularidade = area / cv2.contourArea(box)
+    if retangularidade < 0.65:
+        continue
+
+    # Circularidade modificada
+    # perimeter = cv2.arcLength(cnt, True)
+    # circularidade = 4 * np.pi * area / (perimeter ** 2)
+    # if circularidade < 0.10 or circularidade > 0.35:
+    #     continue
+
+
+    filtered_contours.append(approx)
+
+for quad in filtered_contours:
+    cv2.drawContours(I_output, [quad], -1, (0,255,0), 3)
+
+pts_src = sort_points(filtered_contours[0])
+
+W, H = tamanho_da_placa(pts_src)
+
+pts_dst = np.array([
+        [0, 0],
+        [W - 1, 0],
+        [W - 1, H - 1],
+        [0, H - 1]
+    ], dtype="float32")
+
+# matriz de homografia
+M = cv2.getPerspectiveTransform(pts_src, pts_dst)
+
+# aplica homografia planar
+I_ajusted = cv2.warpPerspective(I_input, M, (W, H))
 
 # OCR com Template Matching
 source_gray = cv2.cvtColor(I_ajusted, cv2.COLOR_BGR2GRAY)
@@ -129,7 +218,7 @@ for char_info in found_chars:
     if is_colon_dot or is_frame:
         continue
     cv2.rectangle(I_output, (x, y), (x + w, y + h), (0, 255, 0), 2)
-
+    
 # Exibição dos resultados
 fig = plt.figure()
 ax1 = fig.add_subplot(1,2,1)
